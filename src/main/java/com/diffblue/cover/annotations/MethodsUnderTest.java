9dce00576ba7322f8d3fae999094868c6608fcd8
Maria Kusber <maria.kusber@diffblue.com>
2024-11-08 12:54:10 +0000

    Move the annotation to the same package
    Javadoc explains the difference between this one and the testing-advice-ones enough, and "generated" was questionable

--- src/main/java/com/diffblue/cover/annotations/MethodsUnderTest.java
+++ src/main/java/com/diffblue/cover/annotations/MethodsUnderTest.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.diffblue.cover.annotations.generated;
+package com.diffblue.cover.annotations;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
