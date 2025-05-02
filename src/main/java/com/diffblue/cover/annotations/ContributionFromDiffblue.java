5a71c6fdf75b10622954c55becc21f92df5c17c1
James Catto <james.catto@diffblue.com>
2025-05-02 14:54:48 +0100

    TG-23029 - added new annotations for @ContributionFromDiffblue and @ManagedByDiffblue

---
+++ src/main/java/com/diffblue/cover/annotations/ContributionFromDiffblue.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2025 Diffblue Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.diffblue.cover.annotations;
+
+/**
+ * Empty interface, required, with the Category annotation, to label JUnit 4 tests in the following
+ * manner: @Category(ContributionFromDiffblue.class).
+ *
+ * <p>Identifies a test that has contributions from Diffblue.
+ */
+public interface ContributionFromDiffblue {}
