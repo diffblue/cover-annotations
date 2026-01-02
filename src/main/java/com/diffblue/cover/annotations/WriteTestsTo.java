d286d09bdc45dfa5f60de60c769f698e3472b56b
Aiden fry <aiden.fry@diffblue.com>
2026-01-02 10:50:03 +0000

    Copyright year update

--- src/main/java/com/diffblue/cover/annotations/WriteTestsTo.java
+++ src/main/java/com/diffblue/cover/annotations/WriteTestsTo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Diffblue Limited.
+ * Copyright 2025-2026 Diffblue Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
